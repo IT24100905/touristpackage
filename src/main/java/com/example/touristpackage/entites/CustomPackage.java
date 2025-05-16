d751ecb8e565f6d0d8616e9c110dfd8df9e3a6b0
IT24102131 <it24102131@my.sliit.lk>
2025-05-16 15:41:06 +0530

    Add entites custompackage

---
+++ src/main/java/com/example/touristpackage/entites/CustomPackage.java
@@ -0,0 +1,33 @@
+package com.example.touristpackage.entites;
+
+public class CustomPackage {
+    private String country;
+    private int flightPrice;
+    private int dayPrice;
+
+    public CustomPackage(String country, int flightPrice, int dayPrice) {
+        this.country = country;
+        this.flightPrice = flightPrice;
+        this.dayPrice = dayPrice;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getFlightPrice() {
+        return flightPrice;
+    }
+
+    public int getDayPrice() {
+        return dayPrice;
+    }
+
+    public void setFlightPrice(int flightPrice) {
+        this.flightPrice = flightPrice;
+    }
+
+    public void setDayPrice(int dayPrice) {
+        this.dayPrice = dayPrice;
+    }
+}
