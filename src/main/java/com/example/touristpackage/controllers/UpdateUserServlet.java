9a2d8ca70e22670450df75a259fda2c383c7393a
IT24100905 <it24100905@my.sliit.lk>
2025-05-18 00:22:41 +0530

    Add jsp

---
+++ src/main/java/com/example/touristpackage/controllers/UpdateUserServlet.java
@@ -0,0 +1,26 @@
+package com.example.touristpackage.controller;
+
+import com.example.touristpackage.entity.UserDetail;
+import com.example.touristpackage.util.AdminUserService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import jakarta.servlet.ServletException;
+import java.io.IOException;
+
+@WebServlet("/updateUser")
+public class UpdateUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String fullname = request.getParameter("fullname");
+        String contact = request.getParameter("contact");
+        String email = request.getParameter("email");
+        String nic = request.getParameter("nic");
+
+        UserDetail updatedUser = new UserDetail(username, fullname, contact, email, nic);
+        AdminUserService.updateUser(updatedUser);
+
+        response.sendRedirect("manageUsers.jsp");
+    }
+}
