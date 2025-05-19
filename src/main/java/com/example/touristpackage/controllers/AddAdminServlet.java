d368ecb916e34a657ef155a87f0bdbcd7e2199de
IT24103096 <IT24103096@my.sliit.lk>
2025-05-19 22:44:01 +0530

    updated code

--- src/main/java/com/example/touristpackage/controllers/AddAdminServlet.java
+++ src/main/java/com/example/touristpackage/controllers/AddAdminServlet.java
@@ -1,4 +1,4 @@
-package com.example.touristpackage.controller;
+package com.example.touristpackage.controllers;
 
 import com.example.touristpackage.util.AdminAccountUtil;
 import jakarta.servlet.ServletException;
