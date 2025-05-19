cc2bc18ad469a05499642b5ec74b26547b349e6f
Thulindi Rathnayake <it24100905@my.sliit.lk>
2025-05-19 21:18:55 +0530

    filepath fix

--- src/main/java/com/example/touristpackage/util/FilePathUtil.java
+++ src/main/java/com/example/touristpackage/util/FilePathUtil.java
@@ -1,12 +1,12 @@
 package com.example.touristpackage.util;
 
 public class FilePathUtil {
-    public static final String USER_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/users.txt";
-    public static final String DESTINATION_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/destinations.txt";
-    public static final String ORDER_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/customorders.txt";
-    public static final String BASIC_PACKAGES_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/basicpackages.txt";
-    public static final String BASIC_ORDER_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/basicorders.txt";
-    public static final String PACKAGE_IMAGE_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/packageimages.txt";
-    public static final String REVIEW_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/reviews.txt";
-    public static final String USER_DETAILS_FILE = "/Users/sishanhewapathirana/Documents/OOP/TouristPackage/data/userdetail.txt";
+    public static final String USER_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\users.txt";
+    public static final String DESTINATION_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\destinations.txt";
+    public static final String ORDER_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\customorders.txt";
+    public static final String BASIC_PACKAGES_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\basicpackages.txt";
+    public static final String BASIC_ORDER_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\basicorders.txt";
+    public static final String PACKAGE_IMAGE_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\packageimages.txt";
+    public static final String REVIEW_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\reviews.txt";
+    public static final String USER_DETAILS_FILE = "C:\\Users\\thuli\\OneDrive\\Desktop\\New folder\\touristpackage\\data\\userdetail.txt";
 }
